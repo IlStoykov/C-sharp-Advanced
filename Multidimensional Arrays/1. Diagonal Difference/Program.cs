459fbeac4cadca0a97ee1c032d3676384ca0ea5a
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2023-09-22 22:39:16 +0300

    Add files via upload

---
+++ Multidimensional Arrays/1. Diagonal Difference/Program.cs
@@ -0,0 +1,19 @@
+﻿int size = int.Parse(Console.ReadLine());
+int primeSum = 0;
+int secondarySum = 0;
+int[,] matrix = new int[size, size];
+for (int r = 0; r < size; r++) 
+{
+    int[] entr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    for (int c = 0; c < size; c++) 
+    {
+        matrix[r, c] = entr[c];
+    }
+}
+for (int i = 0; i < size; i++) 
+{
+    primeSum += matrix[i, i];
+    secondarySum += matrix[i, size-1-i];
+}
+Console.WriteLine(Math.Abs(primeSum - secondarySum));
+
